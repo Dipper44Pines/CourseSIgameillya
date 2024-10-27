f59191204053415dcc257d6f03140eb548f56569
t1mka83 <tsimofeivashkevich@icloud.com>
2024-10-27 15:46:37 +0100

    Бока и стабилизация

--- Game.py
+++ Game.py
@@ -1,11 +1,15 @@
 print('hello world')
 import pygame as pg
 import sys
+
+pg.init()
+
 BLACK=(255,255,255)
 WHITE=(0,0,0)
 
 SCREEN_WIDTH=600
 SCREEN_HEIGHT=800
+def_font = pg.font.Font(pg.font.get_default_font(),30)
 screen=pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 Clock=pg.time.Clock()
 
@@ -54,9 +58,15 @@ while True:
     ball=pg.Rect(BALL_X,BALL_Y,BALL_WIDTH,BALL_HEIGHT)
     pg.draw.rect(screen, (255,0,0),ball)
 
+
+    ball_centre = (BALL_X + BALL_WIDTH/2,BALL_Y + BALL_HEIGHT/2)
+    platform_centre =(PLATFORM_X + PLATFORM_WIDTH/2, PLATFORM_Y + PLATFORM_HEIGHT/2)
+    text_surface = def_font.render('NO TEXTURE',False,(0,255,0))
+
     
     if ball.colliderect(platform):
-        BALL_DIRECTION=BALL_DIRECTION.reflect(pg.math.Vector2(0,1))
+        collision_vector = (ball_centre[0] - platform_centre[0],ball_centre[1] - platform_centre[1])
+        BALL_DIRECTION=pg.math.Vector2(collision_vector).normalize()
     if ball.colliderect(TOP_BORDER):
         BALL_DIRECTION=BALL_DIRECTION.reflect(pg.math.Vector2(0,1))
     if ball.colliderect(BOTTOM_BORDER):
@@ -64,7 +74,7 @@ while True:
     if ball.colliderect(LEFT_BORDER):
         BALL_DIRECTION=BALL_DIRECTION.reflect(pg.math.Vector2(1,0))
     if ball.colliderect(RIGHT_BORDER):
-        BALL_DIRECTION=BALL_DIRECTION.reflect(pg.math.Vector2(1,0))
+        BALL_DIRECTION=BALL_DIRECTION.reflect(pg.math.Vector2(-1,0))
 
     if BALL_Y <0:
         BALL_SPEED =- BALL_SPEED
@@ -72,7 +82,7 @@ while True:
     if BALL_Y >750:
         BALL_SPEED =- BALL_SPEED
         sys.exit()
-
+    screen.blit(text_surface,(20,20))
     pg.display.flip() 
     Clock.tick(FPS) 
 
