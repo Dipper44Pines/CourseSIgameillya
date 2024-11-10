3c3bf80070083c23c158af591ab903c841e6036d
t1mka83 <tsimofeivashkevich@icloud.com>
2024-11-10 15:48:32 +0100

    Добавление картинок ПНГ

--- Game.py
+++ Game.py
@@ -18,21 +18,22 @@ LEFT_BORDER = pg.Rect(0,0,1,SCREEN_HEIGHT)
 RIGHT_BORDER = pg.Rect(SCREEN_WIDTH,0,1,SCREEN_HEIGHT)
 PLATFORM_WIDTH=200
 PLATFORM_HEIGHT=30
+BALL_WIDTH=100
+BALL_HEIGHT=100
 
 
-def game(screen, Clock):
+def game(screen, Clock,assets):
 
 
     PLATFORM_X=(SCREEN_WIDTH+PLATFORM_WIDTH) //2
     PLATFORM_Y=int(SCREEN_HEIGHT*0.75)
     PLATFORM_SPEED=3
-    BALL_WIDTH=40
-    BALL_HEIGHT=40 
+
 
     FPS=120
 
-    BALL_X=50
-    BALL_Y=50
+    BALL_X=100
+    BALL_Y=100
     BALL_SPEED=5
     BALL_DIRECTION=pg.math.Vector2(1,1).normalize()
 
@@ -43,6 +44,7 @@ def game(screen, Clock):
 
 
     while True:
+        BALL_SPEED = COUNTER+1
         for event in pg.event.get():
             if event.type==pg.QUIT:
                 sys.exit()
@@ -61,13 +63,24 @@ def game(screen, Clock):
         BALL_Y+=speed_vector.y
         BALL_X+=speed_vector.x
 
-        screen.fill(WHITE)
+        screen.blit(assets['background'],(0,0))
 
-        platform=pg.Rect(PLATFORM_X,PLATFORM_Y,PLATFORM_WIDTH,PLATFORM_HEIGHT)
-        pg.draw.rect(screen,BLACK,platform)
-        ball=pg.Rect(BALL_X,BALL_Y,BALL_WIDTH,BALL_HEIGHT)
-        pg.draw.rect(screen, (255,0,0),ball)
+        # platform=pg.Rect(PLATFORM_X,PLATFORM_Y,PLATFORM_WIDTH,PLATFORM_HEIGHT)
+        # pg.draw.rect(screen,BLACK,platform)
+        ball = assets['ball'].get_rect()
+        ball.x = BALL_X
+        ball.y = BALL_Y
 
+
+        platform = assets['panel'].get_rect()
+        platform.x = PLATFORM_X
+        platform.y = PLATFORM_Y
+
+
+        # ball=pg.Rect(BALL_X,BALL_Y,BALL_WIDTH,BALL_HEIGHT)
+        # pg.draw.rect(screen, (255,0,0),ball)
+        screen.blit(assets['ball'], ball)
+        screen.blit(assets['panel'], platform)
 
         ball_centre = (BALL_X + BALL_WIDTH/2,BALL_Y + BALL_HEIGHT/2)
         platform_centre =(PLATFORM_X + PLATFORM_WIDTH/2, PLATFORM_Y + PLATFORM_HEIGHT/2)
@@ -88,7 +101,7 @@ def game(screen, Clock):
         if ball.colliderect(RIGHT_BORDER):
             BALL_DIRECTION=BALL_DIRECTION.reflect(pg.math.Vector2(-1,0))
         
-8
+
         screen.blit(text_surface,(20,20))
         pg.display.flip() 
         Clock.tick(FPS)
@@ -97,9 +110,19 @@ def game(screen, Clock):
 
 if __name__ == '__main__':
     screen_out=pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    assets = {
+        
+     'ball': pg.transform.scale(pg.image.load('ball.png').convert_alpha(), (BALL_WIDTH , BALL_HEIGHT)),
+     'background': pg.transform.scale(pg.image.load('background.png').convert(),(SCREEN_WIDTH , SCREEN_HEIGHT)),
+     'panel': pg.transform.scale(pg.image.load('panel.png').convert(),(PLATFORM_WIDTH , PLATFORM_HEIGHT))
+        
+    
+    }
+    BALL_SPRITE_IMAGE = pg.image.load('ball.png').convert()
+    PANEL_SPRITE_IMAGE = pg.image.load('PANEL.png').convert()
     Clock_out=pg.time.Clock()
     while True:
-        game(screen_out, Clock_out)
+        game(screen_out, Clock_out,assets)
 
         finish_text = def_font.render('Game over', False, (0, 255 ,0))
         screen_out.blit(finish_text, (SCREEN_WIDTH/2-30, SCREEN_HEIGHT/2))
